ee84e3d1609d131cc0c96e318ab8dda1a2d80b99
benedek702 <81017415+benedek702@users.noreply.github.com>
2021-04-22 14:29:58 +0200

    Add files via upload

---
+++ Audi A4.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudiA4 : MonoBehaviour
+{
+    private int speed;
+
+    public int Speed
+    {
+        get { return speed; }
+        set { speed = value; }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Speed = 8;
+        print("Sebbesség=8");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
